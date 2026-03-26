42ea554c1a78eb8f57c81d2828ff7a39e4b65040
oshiojay <oshiobughieailakhu@gmail.com>
2026-03-26 21:56:32 +0100

    chnage in the database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://oshiobughieailakhu_db_user:iOiIuik5YZSHgvOn@cluster0.vajd8jb.mongodb.net/').then(()=>{
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
     console.log('Connected to MongoDB')
 }).catch((err)=>{
     console.error('Error connecting to MongoDB:', err)
